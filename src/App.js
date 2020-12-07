2558152e8bee6a88d471324d5d86d0682bd88602
Elad Yair <elady@r2net.com>
2020-12-07 09:43:20 +0200

    Adding bootstarp styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
+
 // Styles
 import "./App.css";
 
 // Components
+import GuessedWords from "./components/guessed-words/guessed-words";
+import Congrats from "./components/congrats/congrats";
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Jotto</h1>
+            <Congrats success={true} />
+            <GuessedWords guessedWords={[]} />
+        </div>
+    );
+};
 
 export default App;
