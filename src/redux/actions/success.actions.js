c032a2d407ba04d4dbea725a491e54d561be36a2
Elad Yair <elady@r2net.com>
2020-12-08 18:33:13 +0200

    Adding first redux action and test for it

---
+++ src/redux/actions/success.actions.js
@@ -0,0 +1,7 @@
+import { CORRECT_GUESS } from '../types';
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type 'CORRECT_GUESS'
+ */
+export const correctGuess = () => ({});
---
+++ src/redux/actions/success.test.js
@@ -0,0 +1,9 @@
+import { CORRECT_GUESS } from '../types';
+import { correctGuess } from './success.actions';
+
+describe('correctGuess', () => {
+    test("Should return an action with type 'CORRECT_GUESS'", () => {
+        const action = correctGuess();
+        expect(action).toBe({});
+    });
+});
